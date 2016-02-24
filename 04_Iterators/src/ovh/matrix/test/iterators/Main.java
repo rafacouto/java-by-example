01afda787ff35b6d1ff4fc2c7dfe7bc657191953
Rafa Couto <rafacouto@gmail.com>
2016-02-24 17:16:59 +0100

    Exercicios 04-08.

---
+++ 04_Iterators/src/ovh/matrix/test/iterators/Main.java
@@ -0,0 +1,16 @@
+package ovh.matrix.test.iterators;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int counter = 6;
+        while (counter-- > 0) {
+            System.out.println("Valor de counter: " + counter);
+        }
+
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Valor de i: " + i);
+        }
+    }
+}
---
+++ 08_Polimorfismo/src/ovh/matrix/test/Main.java
@@ -0,0 +1,8 @@
+package ovh.matrix.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
