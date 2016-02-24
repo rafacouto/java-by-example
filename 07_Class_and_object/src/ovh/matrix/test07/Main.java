c428272455bc1161a678af4270aff463bbaf0460
Rafa Couto <rafacouto@gmail.com>
2016-02-24 18:53:42 +0100

    07_update

---
+++ 07_Class_and_object/src/ovh/matrix/test07/Main.java
@@ -0,0 +1,19 @@
+package ovh.matrix.test07;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Coche seat = new Coche();
+
+        seat.setColor("gris");
+        seat.setMarca("Seat");
+        seat.setModelo("Ibiza");
+        seat.setPuertas(3);
+        seat.setRuedas(4);
+
+        Pez pez1 = new Pez(10, 1, 3, true);
+        pez1.comunicar();
+        pez1.nadar();
+    }
+}
---
+++ 07_Class_and_object/src/ovh/matrix/test07/Pez.java
@@ -0,0 +1,40 @@
+package ovh.matrix.test07;
+
+/**
+ * Created by caligari on 23/02/2016.
+ */
+public class Pez extends Animal {
+
+    int aletas;
+    boolean medioSalado;
+
+    public boolean isMedioSalado() {
+        return medioSalado;
+    }
+
+    public void setMedioSalado(boolean medioSalado) {
+        this.medioSalado = medioSalado;
+    }
+
+    public int getAletas() {
+        return aletas;
+    }
+
+    public void setAletas(int aletas) {
+        this.aletas = aletas;
+    }
+
+    public Pez(int tamanho, int peso, int aletas, boolean medioSalado) {
+
+        super.setNombre("Pez");
+        super.setTamanho(tamanho);
+        super.setPeso(peso);
+
+        this.aletas = aletas;
+        this.medioSalado = medioSalado;
+    }
+
+    public void nadar() {
+        System.out.println("Pez: nadando...");
+    }
+}
