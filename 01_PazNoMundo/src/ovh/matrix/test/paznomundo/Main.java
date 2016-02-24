0077d66a33281398e0459357e9e0338c43f70a8c
Rafa Couto <rafacouto@gmail.com>
2016-02-24 17:47:33 +0100

    09_Abstract_Class

--- 01_PazNoMundo/src/ovh/matrix/test/paznomundo/Main.java
+++
@@ -1,8 +0,0 @@
-package ovh.matrix.test.paznomundo;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Paz no mundo aos homes e mulleres de boa vontade!");
-    }
-}
--- 08_Polimorfismo/src/ovh/matrix/test/Main.java
+++
@@ -1,8 +0,0 @@
-package ovh.matrix.test;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
